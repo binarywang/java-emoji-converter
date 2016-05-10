808445fe355923679764c636170a70602eafcbdc
王彬 <wangbin@ucredit.com>
2016-05-10 16:24:18 +0800

    try to read xml first from url

--- src/main/java/cn/binarywang/java/emoji/EmojiReader.java
+++ src/main/java/cn/binarywang/java/emoji/EmojiReader.java
@@ -1,5 +1,8 @@
 package cn.binarywang.java.emoji;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
 import org.apache.commons.lang3.builder.ToStringBuilder;
 
 import com.thoughtworks.xstream.XStream;
@@ -8,15 +11,27 @@ import com.thoughtworks.xstream.io.xml.DomDriver;
 import cn.binarywang.java.emoji.model.Emoji4Unicode;
 
 public class EmojiReader {
+    private static final String url = "https://raw.githubusercontent.com/googlei18n/emoji4unicode/master/data/emoji4unicode.xml";
 
     public static Emoji4Unicode read() {
         XStream xstream = new XStream(new DomDriver());
         xstream.processAnnotations(Emoji4Unicode.class);
 
-        Emoji4Unicode a = (Emoji4Unicode) xstream.fromXML(
-            ClassLoader.getSystemResourceAsStream("emoji4unicode.xml"));
+        Emoji4Unicode emoji = null;
+        try {
+            emoji = (Emoji4Unicode) xstream.fromXML(new URL(url));
+            System.err.println("==============from url==============");
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
 
-        return a;
+        if (emoji == null) {
+            System.err.println("==============from local file==============");
+            return  (Emoji4Unicode) xstream.fromXML(
+            ClassLoader.getSystemResourceAsStream("emoji4unicode.xml"));
+        }
+
+        return emoji;
     }
 
     public static void main(String[] args) {
